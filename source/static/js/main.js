8891c243bbd471bda5922d0f78d758818da9df42
Pedro Leme <pedrocicoleme@yahoo.com>
2015-11-02 11:14:43 -0200

    changed routes to use states via ui.router

--- source/static/js/main.js
+++ source/static/js/main.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
     //$("#live_stream_img").attr("src",$("#live_stream_img").data("src"));
 });
 
-var app = angular.module('easyvideApp', ['ui.materialize', 'ngRoute']);
+var app = angular.module('easyvideApp', ['ui.materialize', 'ngRoute', 'ui.router']);
 
 // this way there is no conflict between jinja and angularjs
 app.config(['$interpolateProvider', function($interpolateProvider) {
@@ -13,24 +13,41 @@ app.config(['$interpolateProvider', function($interpolateProvider) {
     $interpolateProvider.endSymbol(']]');
 }]);
 
-
-app.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.
-    when("/cameras", {templateUrl: "static/partials/cameras.html", controller: "CamerasCtrl"}).
-    when("/drivers/:id", {templateUrl: "partials/driver.html", controller: "driverController"}).
-    otherwise({redirectTo: '/drivers'});
-}]);
+app.config(function($stateProvider, $routeProvider) {
+    //
+    // For any unmatched url, redirect to /state1
+    $routeProvider.otherwise("/cameras");
+    //
+    // Now set up the states
+    $stateProvider
+    .state('cameras', {
+        url: '/cameras',
+        templateUrl: 'static/partials/cameras.html',
+        controller: 'CamerasCtrl'
+    })
+    .state('cameras.add_new', {
+        url: '/add_new',
+        templateUrl: 'static/partials/cameras.add_new.html',
+        controller: function($scope) {
+            console.log('where is the new camera?');
+            alert('new camera modal should appear');
+        }
+    })
+    .state('state2', {
+        url: "/state2",
+        templateUrl: "partials/state2.html"
+    })
+    .state('state2.list', {
+        url: "/list",
+        templateUrl: "partials/state2.list.html",
+        controller: function($scope) {
+        $scope.things = ["A", "Set", "Of", "Things"];
+        }
+    });
+});
 
 app.controller('CamerasCtrl', function ($scope, $http, $routeParams) {
-    var init = function () {
-        $scope.loadCameras();
-
-        if ($routeParams.add_new) {
-            $scope.addCamera();
-        }
-    };
-    init();
-
+    alert('hello, I\'m CamerasCtrl')
     $scope.loadCameras = function () {
         $http.get('/api/camera/list').
         success(function(data, status, headers, config) {
@@ -47,4 +64,15 @@ app.controller('CamerasCtrl', function ($scope, $http, $routeParams) {
     };
 
     // $scope.addCamera();
+
+    var init = function () {
+        $scope.loadCameras();
+
+        if ($routeParams.add_new) {
+            $scope.addCamera();
+        }
+
+        alert('hei');
+    };
+    init();
 });
