ccb567a7df25242cc7144d24c2c1f41a2f00ac09
Pedro Leme <pedrocicoleme@yahoo.com>
2015-11-07 18:12:29 -0200

    restart capture on error

--- source/view_camera.py
+++ source/view_camera.py
@@ -78,88 +78,97 @@ def detect_motion(camera):
 
     print u'starting detection...'
 
-    # initialize the camera and grab a reference to the raw camera capture
-    camera = get_cap(input_resource)
-
-    global framebuffers
-    
-    # allow the camera to warmup, then initialize the average frame, last
-    # uploaded timestamp, and frame motion counter
-    print u'[INFO] warming up...'
-    time.sleep(conf[u'camera_warmup_time'])
-    avg = None
-    lastUploaded = datetime.datetime.now()
-    motionCounter = 0
-
-    last_time = time.time()
-
-    # capture frames from the camera
     while True:
-        # grab the raw NumPy array representing the image and initialize
-        # the timestamp and occupied/unoccupied text
+        try:
+            # initialize the camera and grab a reference to the raw camera capture
+            camera = get_cap(input_resource)
 
-        sleep_interval = spf - (time.time() - last_time)
-        if sleep_interval > 0:
-            time.sleep(0.020)
-            camera.grab()
-            continue
+            global framebuffers
+            
+            # allow the camera to warmup, then initialize the average frame, last
+            # uploaded timestamp, and frame motion counter
+            print u'[INFO] warming up...'
+            time.sleep(conf[u'camera_warmup_time'])
+            avg = None
+            lastUploaded = datetime.datetime.now()
+            motionCounter = 0
 
-        last_time = time.time()
+            last_time = time.time()
 
-        grabbed, frame = camera.read()
+            # capture frames from the camera
+            while True:
+                # grab the raw NumPy array representing the image and initialize
+                # the timestamp and occupied/unoccupied text
 
-        timestamp = datetime.datetime.now()
-        text = u'Unoccupied'
-     
-        # resize the frame, convert it to grayscale, and blur it
-        frame = imutils.resize(frame, width=500)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        gray = cv2.GaussianBlur(gray, (21, 21), 0)
-     
-        # if the average frame is None, initialize it
-        if avg is None:
-            print u'[INFO] starting background model...'
-            avg = gray.copy().astype(u'float')
-            continue
-     
-        # accumulate the weighted average between the current frame and
-        # previous frames, then compute the difference between the current
-        # frame and running average
-        cv2.accumulateWeighted(gray, avg, 0.5)
-        frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
+                sleep_interval = spf - (time.time() - last_time)
+                if sleep_interval > 0:
+                    time.sleep(0.020)
+                    camera.grab()
+                    continue
 
-        # threshold the delta image, dilate the thresholded image to fill
-        # in holes, then find contours on thresholded image
-        thresh = cv2.threshold(frameDelta, conf[u'delta_thresh'], 255, cv2.THRESH_BINARY)[1]
-        thresh = cv2.dilate(thresh, None, iterations=2)
-        (cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-     
-        # loop over the contours
-        for c in cnts:
-            # if the contour is too small, ignore it
-            if cv2.contourArea(c) < conf[u'min_area']:
-                continue
-     
-            # compute the bounding box for the contour, draw it on the frame,
-            # and update the text
-            (x, y, w, h) = cv2.boundingRect(c)
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            text = u'Occupied'
-     
-        # draw the text and timestamp on the frame
-        ts = timestamp.strftime(u'%A %d %B %Y %H:%M:%S')
-        cv2.putText(frame, u'Room Status: {}'.format(text), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-        cv2.putText(frame, ts, (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+                last_time = time.time()
 
-        #cv2.imshow(u'frame', frame)
-        frame2 = im = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                grabbed, frame = camera.read()
 
-        # create a CLAHE object (Arguments are optional).
-        clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
-        cl1 = clahe.apply(frame2)
+                timestamp = datetime.datetime.now()
+                text = u'Unoccupied'
+             
+                # resize the frame, convert it to grayscale, and blur it
+                frame = imutils.resize(frame, width=500)
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                gray = cv2.GaussianBlur(gray, (21, 21), 0)
+             
+                # if the average frame is None, initialize it
+                if avg is None:
+                    print u'[INFO] starting background model...'
+                    avg = gray.copy().astype(u'float')
+                    continue
+             
+                # accumulate the weighted average between the current frame and
+                # previous frames, then compute the difference between the current
+                # frame and running average
+                cv2.accumulateWeighted(gray, avg, 0.5)
+                frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
 
-        ret, jpeg = cv2.imencode('.jpg', cl1)
-        framebuffers[sid][u'queue'].appendleft(jpeg.tobytes())
+                # threshold the delta image, dilate the thresholded image to fill
+                # in holes, then find contours on thresholded image
+                thresh = cv2.threshold(frameDelta, conf[u'delta_thresh'], 255, cv2.THRESH_BINARY)[1]
+                thresh = cv2.dilate(thresh, None, iterations=2)
+                (cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+             
+                # loop over the contours
+                for c in cnts:
+                    # if the contour is too small, ignore it
+                    if cv2.contourArea(c) < conf[u'min_area']:
+                        continue
+             
+                    # compute the bounding box for the contour, draw it on the frame,
+                    # and update the text
+                    (x, y, w, h) = cv2.boundingRect(c)
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                    text = u'Occupied'
+             
+                # draw the text and timestamp on the frame
+                ts = timestamp.strftime(u'%A %d %B %Y %H:%M:%S')
+                cv2.putText(frame, u'Room Status: {}'.format(text), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+                cv2.putText(frame, ts, (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+
+                #cv2.imshow(u'frame', frame)
+                frame2 = im = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+                # create a CLAHE object (Arguments are optional).
+                clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+                cl1 = clahe.apply(frame2)
+
+                ret, jpeg = cv2.imencode('.jpg', cl1)
+                framebuffers[sid][u'queue'].appendleft(jpeg.tobytes())
+
+                if not get_run_state() or check_should_refresh():
+                    break
+        except Exception as e:
+            print e
+
+            time.sleep(1)
 
         if not get_run_state() or check_should_refresh():
             break
@@ -178,10 +187,10 @@ def cam_threads_controller():
     while True:
         if get_run_state():
             if check_should_refresh:
-                check_should_refresh(False)
-
                 for sid, framebuffer in framebuffers.iteritems():
                     framebuffer[u'thread'].join()
+
+                check_should_refresh(False)
 
                 for camera in get_cameras_list():
                     sid = u'%s' % camera[u'cameraID']
