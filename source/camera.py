b31c2f5b8278219e0911e8cc941ff2528ac23261
Pedro <pedrocicoleme@gmail.com>
2015-12-30 16:43:39 -0200

    camera running now inside thread

--- source/camera.py
+++ source/camera.py
@@ -2,11 +2,20 @@
 
 from flask import Blueprint, request, jsonify, Response
 
-import time, logging
-import view_camera
+import time, datetime, logging
+
+from threading import Thread
+
+import numpy as np
+import cv2
+import imutils
+
+from collections import deque
 
 from sqlalchemy import create_engine, Table, MetaData
 from sqlalchemy.orm import scoped_session, sessionmaker
+
+running_cameras = {}
 
 engine = create_engine('sqlite:///easyvide.db', convert_unicode=True)
 db = scoped_session(sessionmaker(bind=engine))
@@ -70,6 +79,200 @@ def get_cameras_list():
 
     return cameras_list
 
+conf = {
+    u'show_video': True,
+    u'use_dropbox': True,
+    u'dropbox_key': u'YOUR_DROPBOX_KEY',
+    u'dropbox_secret': u'YOUR_DROPBOX_SECRET',
+    u'dropbox_base_path': u'YOUR_DROPBOX_PATH',
+    u'min_upload_second': 3.0,
+    u'min_motion_frames': 1,
+    u'camera_warmup_time': 2.5,
+    u'delta_thresh': 10,
+    u'resolution': [640, 480],
+    u'fps': 3,
+    u'min_area': 1000}
+
+running_cameras = {}
+
+class Camera(Thread):
+    def __init__(self, cam):
+        Thread.__init__(self)
+
+        self.cameraID = cam[u'cameraID']
+        self.source = cam[u'source']
+        self.fps = cam[u'fps']
+        self.spf = 1.0 / self.fps
+        self.res_w = cam[u'res_w']
+        self.res_h = cam[u'res_h']
+        self.resolution = u'%sx%s' % (cam[u'res_w'], cam[u'res_h'])
+
+        self.cap = None
+
+        self.queue_capture = deque([], 10*self.fps)
+        self.queue_analysis = deque([], 10*self.fps)
+
+        self.status = False
+
+    def __del__(self):
+        del self.cap
+
+    def get_cap(self):
+        if self.cap:
+            return self.cap
+
+        if unicode(self.source).isdecimal():
+            input_resource = int(self.source)
+        else:
+            input_resource = self.source
+
+        self.cap = cv2.VideoCapture(input_resource)
+
+        return self.cap
+
+    def detect_motion(self):
+        print u'starting detection for cameraID %s...' % self.cameraID
+
+        while True:
+            try:
+                # initialize the camera and grab a reference to the raw camera capture
+                camera = self.get_cap()
+                
+                # allow the camera to warmup, then initialize the average frame, last
+                # uploaded timestamp, and frame motion counter
+                print u'[INFO] warming up...'
+                time.sleep(conf[u'camera_warmup_time'])
+                avg = None
+                lastUploaded = datetime.datetime.now()
+                motionCounter = 0
+
+                last_time = time.time()
+
+                # capture frames from the camera
+                while True:
+                    # grab the raw NumPy array representing the image and initialize
+                    # the timestamp and occupied/unoccupied text
+
+                    sleep_interval = self.spf - (time.time() - last_time)
+                    if sleep_interval > 0:
+                        time.sleep(0.020)
+                        camera.grab()
+                        continue
+
+                    last_time = time.time()
+
+                    grabbed, frame = camera.read()
+
+                    if not grabbed:
+                        self.status = False
+
+                        time.sleep(3)
+                        break
+                    else:
+                        self.status = True
+
+                    timestamp = datetime.datetime.now()
+                    text = u'Unoccupied'
+                 
+                    # resize the frame, convert it to grayscale, and blur it
+                    frame = imutils.resize(frame, width=500)
+                    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+                 
+                    # if the average frame is None, initialize it
+                    if avg is None:
+                        print u'[INFO] starting background model...'
+                        avg = gray.copy().astype(u'float')
+                        continue
+                 
+                    # accumulate the weighted average between the current frame and
+                    # previous frames, then compute the difference between the current
+                    # frame and running average
+                    cv2.accumulateWeighted(gray, avg, 0.5)
+                    frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
+
+                    # threshold the delta image, dilate the thresholded image to fill
+                    # in holes, then find contours on thresholded image
+                    thresh = cv2.threshold(frameDelta, conf[u'delta_thresh'], 255, cv2.THRESH_BINARY)[1]
+                    thresh = cv2.dilate(thresh, None, iterations=2)
+                    (cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+                 
+                    # loop over the contours
+                    for c in cnts:
+                        # if the contour is too small, ignore it
+                        if cv2.contourArea(c) < conf[u'min_area']:
+                            continue
+                 
+                        # compute the bounding box for the contour, draw it on the frame,
+                        # and update the text
+                        (x, y, w, h) = cv2.boundingRect(c)
+                        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                        text = u'Occupied'
+                 
+                    # draw the text and timestamp on the frame
+                    ts = timestamp.strftime(u'%A %d %B %Y %H:%M:%S')
+                    cv2.putText(frame, u'Room Status: {}'.format(text), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+                    cv2.putText(frame, ts, (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+
+                    #cv2.imshow(u'frame', frame)
+                    frame2 = im = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+                    # create a CLAHE object (Arguments are optional).
+                    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+                    cl1 = clahe.apply(frame2)
+
+                    ret, jpeg = cv2.imencode('.jpg', cl1)
+                    self.queue_analysis.appendleft(jpeg.tobytes())
+
+                    if not get_run_state() or check_should_refresh():
+                        break
+            except Exception as e:
+                logging.exception(u'error trying to get picture')
+
+                self.status = False
+
+                time.sleep(1)
+
+            if not get_run_state() or check_should_refresh():
+                break
+
+        self.cap.release()
+        self.cap = None
+
+    def run(self):
+        self.detect_motion()
+
+#input_resource = u'http://192.168.1.8:81/videostream.cgi?user=admin&password=admin&rate=2&x=.mjpeg'
+#input_resource = u'rtsp://admin:admin@192.168.1.8:81/h.264.sdp?x=tcp'
+input_resource = u'http://192.168.1.8:81/videostream.asf?user=admin&password=admin'
+
+def cam_threads_controller():
+    global running_cameras
+
+    while True:
+        if get_run_state():
+            if check_should_refresh():
+                for sid, running_camera in running_cameras.iteritems():
+                    running_camera.join()
+                    print u'retornou a cameraID %s' % sid
+
+                check_should_refresh(False)
+
+                for camera in get_cameras_list():
+                    sid = u'%s' % camera[u'cameraID']
+
+                    running_cameras[sid] = Camera(camera)
+                    running_cameras[sid].daemon = True
+                    running_cameras[sid].start()
+            else:
+                time.sleep(1)
+        else:
+            time.sleep(1)
+
+threads_master = Thread(target=cam_threads_controller)
+threads_master.daemon = True
+threads_master.start()
+
 @camera.route(u'/api/state')
 @camera.route(u'/api/state/<int:state>')
 def set_get_state(state=None):
@@ -108,7 +311,6 @@ def upsert_camera(cameraID=None):
         cam[u'res_h'] = 480
 
     result = {}
-    # try:
     if not cameraID:
         cam[u'cameraID'] = None
         ins = cameraTbl.insert().values(cam).execute()
@@ -150,10 +352,7 @@ def delete_camera(cameraID):
     
     return jsonify(**result)
 
-fps = 1
-spf = 1.0 / fps
-
-def live_stream_helper(cameraID, fps=fps):
+def live_stream_helper(cameraID, fps=1):
     sid = u'%s' % cameraID
     
     spf = 1.0 / fps
@@ -168,8 +367,8 @@ def live_stream_helper(cameraID, fps=fps):
         last_time = time.time()
         
         try:
-            if view_camera.framebuffers[sid][u'status']:
-                frame = view_camera.framebuffers[sid][u'queue'][0]
+            if running_cameras[sid].status:
+                frame = running_cameras[sid].queue_analysis[0]
 
                 yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
